8ae3dbe233b1e0dd1b050fa4412c70710bae5545
monti-python <32843378+monti-python@users.noreply.github.com>
2022-12-06 01:20:34 +0100

    Add function to get the top n sessions by duration

--- lastfm_pyspark/etls.py
+++ lastfm_pyspark/etls.py
@@ -3,11 +3,12 @@ from pyspark.sql import functions as F
 from pyspark.sql.dataframe import DataFrame as DF
 from pyspark.sql import window as W
 
-def assign_session_id(df: DF, threshold=1200, user_col="user_id", time_col="timestamp"):
+def assign_session_id(df: DF, threshold: int, user_col: str, time_col: str):
     """
     Assigns a session id to each row in a dataframe.
     A session is defined as a period of time where the user is active.
-    The user is considered active if the time between two consecutive events is less than the threshold.
+    The user is considered active if the time between two consecutive events
+    is less than the threshold.
     The session id is a monotonically increasing integer.
     
     Parameters
@@ -32,10 +33,44 @@ def assign_session_id(df: DF, threshold=1200, user_col="user_id", time_col="time
         df
         .select(
             '*',
-            (F.col(time_col) - F.lag(time_col).over(w)).cast("long").alias('inactive_time'),
+            (F.col(time_col) - F.lag(time_col).over(w)).cast("long")
+            .alias('inactive_time'),
         )
         .select(
             *df.columns,
-            F.sum(F.when(F.col('inactive_time') > threshold, 1).otherwise(0)).over(w).alias('session_id'),
+            F.sum(F.when(F.col('inactive_time') > threshold, 1)
+            .otherwise(0)).over(w).alias('session_id'),
         )
+    )
+
+def get_top_sessions(df: DF, user_col: str, session_col: str, time_col: str, n: int):
+    """
+    Returns the top n sessions by duration.
+    
+    Parameters
+    ----------
+    df : pyspark.sql.dataframe.DataFrame
+        The dataframe to get the top sessions from.
+        user_col : str
+        The name of the user column.
+        session_col : str
+        The name of the session column.
+        time_col : str
+        The name of the time column.
+        n : int
+        The number of sessions to return.
+        
+        Returns
+        -------
+        pyspark.sql.dataframe.DataFrame
+        The top n sessions.
+    """
+    return (
+        df
+        .groupBy(user_col, session_col)
+        .agg(
+            (F.max(time_col) - F.min(time_col)).cast('long').alias('session_duration'),
+        )
+        .orderBy(F.col('session_duration').desc())
+        .limit(n)
     )
--- test/test_etls.py
+++ test/test_etls.py
@@ -5,7 +5,7 @@ from pyspark.sql import window as W
 from datetime import datetime
 import pytest
 
-from lastfm_pyspark.etls import assign_session_id
+from lastfm_pyspark.etls import assign_session_id, get_top_sessions
 
 
 def test_assign_session_id(spark):
@@ -22,13 +22,13 @@ def test_assign_session_id(spark):
         ("user2", datetime.fromisoformat("2020-01-01 00:20:00")),
         ("user2", datetime.fromisoformat("2020-01-01 00:45:00")),
     ], schema=schema)
-    df = assign_session_id(
+    res = assign_session_id(
         df,
         threshold=1200,
         user_col="user_id",
         time_col="timestamp"
     )
-    expected = spark.createDataFrame([
+    exp = spark.createDataFrame([
         ("user1", datetime.fromisoformat("2020-01-01 00:00:00"), 0),
         ("user1", datetime.fromisoformat("2020-01-01 00:15:00"), 0),
         ("user1", datetime.fromisoformat("2020-01-01 00:30:00"), 0),
@@ -37,4 +37,39 @@ def test_assign_session_id(spark):
         ("user2", datetime.fromisoformat("2020-01-01 00:20:00"), 0),
         ("user2", datetime.fromisoformat("2020-01-01 00:45:00"), 1),
     ], schema=schema.add("session_id", T.IntegerType()))
-    assert df.collect() == expected.collect()
+    assert res.collect() == exp.collect()
+
+def test_get_top_sessions(spark):
+    schema_in = T.StructType([
+        T.StructField("user_id", T.StringType(), True),
+        T.StructField("session_id", T.IntegerType(), True),
+        T.StructField("timestamp", T.TimestampType(), True),
+    ])
+    schema_out = T.StructType([
+        T.StructField("user_id", T.StringType(), True),
+        T.StructField("session_id", T.IntegerType(), True),
+        T.StructField("session_duration", T.LongType(), True),
+    ])
+
+    df = spark.createDataFrame([
+        ("user1", 0, datetime.fromisoformat("2020-01-01 00:00:00")),
+        ("user1", 0, datetime.fromisoformat("2020-01-01 00:15:00")),
+        ("user1", 0, datetime.fromisoformat("2020-01-01 00:30:00")),
+        ("user1", 1, datetime.fromisoformat("2020-01-01 00:55:00")),
+        ("user2", 0, datetime.fromisoformat("2020-01-01 00:10:00")),
+        ("user2", 0, datetime.fromisoformat("2020-01-01 00:20:00")),
+        ("user2", 1, datetime.fromisoformat("2020-01-01 00:45:00")),
+    ], schema=schema_in)
+
+    res = get_top_sessions(
+        df,
+        user_col="user_id",
+        session_col="session_id",
+        time_col="timestamp",
+        n=2
+    )
+    exp = spark.createDataFrame([
+        ("user1", 0, 60*30),
+        ("user2", 0, 60*10),
+    ], schema=schema_out)
+    assert res.collect() == exp.collect()
