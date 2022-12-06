5f2545720dc815c01916d87a6a12b693cf15301f
monti-python <32843378+monti-python@users.noreply.github.com>
2022-12-06 01:02:20 +0100

    Consistent quoting

--- test/conftest.py
+++ test/conftest.py
@@ -5,11 +5,11 @@ import pytest
 @pytest.fixture(scope='session')
 def spark():
     conf = SparkConf().setAll([
-        ('spark.default.parallelism', '1'),
-        ('spark.driver.memory', '1g'),
-        ('spark.executor.memory', '1g'),
-        ('spark.ui.enabled', 'false'),
-        ('spark.driver.maxResultSize', '1g'),
+        ("spark.default.parallelism", "1"),
+        ("spark.driver.memory", "1g"),
+        ("spark.executor.memory", "1g"),
+        ("spark.ui.enabled", "false"),
+        ("spark.driver.maxResultSize", "1g"),
     ])
     spark = SparkSession.builder.master("local[*]").config(conf=conf).getOrCreate()
     yield spark
